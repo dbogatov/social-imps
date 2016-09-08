70cbd1b68b1206ccff478a994737b7caa9d7c438
Dmytro <dbogatov@wpi.edu>
2016-09-07 20:52:22 -0400

    Add 777 page.

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.sendFile('777.html', { root: __dirname });
 });
 
 module.exports = router;
