696548d1f4d35aa3446aed2a7e870e8fef6c3d0f
Dmytro <dbogatov@wpi.edu>
2016-09-10 18:36:55 -0400

    Add bootstrap template.

--- Website/app/controllers/home.js
+++ Website/app/controllers/home.js
@@ -1,16 +1,20 @@
 var express = require('express'),
-  router = express.Router(),
-  db = require('../models');
+	router = express.Router(),
+	db = require('../models');
 
 module.exports = function (app) {
-  app.use('/', router);
+	app.use('/', router);
 };
 
 router.get('/', function (req, res, next) {
-  db.Article.findAll().then(function (articles) {
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
-  });
+	res.render('index', {
+		title: "Home"
+	});
+
+	// db.Article.findAll().then(function (articles) {
+	// 	res.render('index', {
+	// 		title: 'Generator-Express MVC',
+	// 		articles: articles
+	// 	});
+	// });
 });
