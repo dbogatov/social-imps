d66dd13717fb60647613904276499a6597d9f7c0
Dmytro <dbogatov@wpi.edu>
2016-09-10 18:15:24 -0400

    Indent.

--- Website/app/models/article.js
+++ Website/app/models/article.js
@@ -3,19 +3,19 @@
 
 module.exports = function (sequelize, DataTypes) {
 
-  var Article = sequelize.define('Article', {
-    title: DataTypes.STRING,
-    url: DataTypes.STRING,
-    text: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function (models) {
-        // example on how to add relations
-        // Article.hasMany(models.Comments);
-      }
-    }
-  });
+	var Article = sequelize.define('Article', {
+		title: DataTypes.STRING,
+		url: DataTypes.STRING,
+		text: DataTypes.STRING
+	}, {
+			classMethods: {
+				associate: function (models) {
+					// example on how to add relations
+					// Article.hasMany(models.Comments);
+				}
+			}
+		});
 
-  return Article;
+	return Article;
 };
 
