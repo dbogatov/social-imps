2795809f1968693bdc1ca5b34884696ff967341b
Dmytro <dbogatov@wpi.edu>
2016-09-10 18:50:12 -0400

    Add Google Analytics.

---
+++ Website/.deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
+
+git fetch origin > /dev/null 2>> deploy.log
+git reset --hard origin/master > /dev/null 2>> deploy.log
+
+npm install > /dev/null 2>> deploy.log
+bower install > /dev/null 2>> deploy.log
+gulp > /dev/null 2>> deploy.log
+
+curl --request POST 'https://push.dbogatov.org/api/push/deploy' --data "project=CV-Website"
--- Website/gulpfile.js
+++ Website/gulpfile.js
@@ -1,29 +1,29 @@
-var gulp = require('gulp'),
-	nodemon = require('gulp-nodemon'),
-	plumber = require('gulp-plumber'),
-	livereload = require('gulp-livereload'),
-	less = require('gulp-less');
+var gulp = require("gulp"),
+	nodemon = require("gulp-nodemon"),
+	plumber = require("gulp-plumber"),
+	livereload = require("gulp-livereload"),
+	less = require("gulp-less");
 
-gulp.task('less', function () {
-	gulp.src('./public/css/*.less')
+gulp.task("less", function () {
+	gulp.src("./public/css/*.less")
 		.pipe(plumber())
 		.pipe(less())
-		.pipe(gulp.dest('./public/css'))
+		.pipe(gulp.dest("./public/css"))
 		.pipe(livereload());
 });
 
-gulp.task('watch', function () {
-	gulp.watch('./public/css/*.less', ['less']);
+gulp.task("watch", function () {
+	gulp.watch("./public/css/*.less", ["less"]);
 });
 
-gulp.task('develop', function () {
+gulp.task("develop", function () {
 	livereload.listen();
 	nodemon({
-		script: 'app.js',
-		ext: 'js coffee handlebars',
+		script: "app.js",
+		ext: "js coffee handlebars",
 		stdout: false
-	}).on('readable', function () {
-		this.stdout.on('data', function (chunk) {
+	}).on("readable", function () {
+		this.stdout.on("data", function (chunk) {
 			if (/^Express server listening on port/.test(chunk)) {
 				livereload.changed(__dirname);
 			}
@@ -33,8 +33,10 @@ gulp.task('develop', function () {
 	});
 });
 
-gulp.task('default', [
-	'less',
-	'develop',
-	'watch'
+gulp.task("default", [
+	"less",
+	"develop",
+	"watch"
 ]);
+
+gulp.task("deploy", ["less"]);
