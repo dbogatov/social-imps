a6d9995670d0a2e846c1d76943e7c6f0e0a3ad81
Dmytro <dbogatov@wpi.edu>
2016-09-11 16:17:18 -0400

    Fix.

--- Website/.deploy.sh
+++ Website/.deploy.sh
@@ -19,4 +19,6 @@ npm install > /dev/null 2>> deploy.log
 bower install --allow-root > /dev/null 2>> deploy.log
 gulp deploy > /dev/null 2>> deploy.log
 
+supervisorctl socialimps restart
+
 curl --request POST 'https://push.dbogatov.org/api/push/deploy' --data "project=Social-Imps"
